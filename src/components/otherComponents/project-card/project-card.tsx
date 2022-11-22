df9e455e95f06c69362668eb72f943038a59883c
SimonKaresz <kareszkaresz0706@gmail.com>
2022-11-22 12:58:06 +0100

    debugging 1.2

--- src/components/otherComponents/project-card/project-card.tsx
+++ src/components/otherComponents/project-card/project-card.tsx
@@ -9,10 +9,10 @@ export const ProjectCard = (props: ProjectCardData) => {
         className="w-[400px] h-[250px] max-[380px]:w-[355px]"
       />
       <div className="description">
-        <h1 className="text-3xl mx-[5px] font-semibold mb-5 text-blue-800/80 select-none">
+        <h1 className="text-3xl font-semibold mb-5 text-blue-800/80 select-none">
           {props.name}
         </h1>
-        <p className="text-lg mb-5 mx-[5px] select-none">{props.description}</p>
+        <p className="text-lg mb-5 select-none">{props.description}</p>
         <a
           className="cursor-ponter py-2 px-1 w-16 border-2 mx-auto border-blue-400 rounded-xl hover:bg-gradient-to-r from-blue-500 to-blue-400"
           href={props.appURL}
